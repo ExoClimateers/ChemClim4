6368d7ef7f412377ba5b06736863285987a68ec5
Joshua Parsons <mucephie@my.yorku.ca>
2023-06-08 18:24:46 -0400

    more work on run script

--- CHEMCLIM/mk_run.py
+++ CHEMCLIM/mk_run.py
@@ -4,7 +4,13 @@ import sys
 test_dir = '~/ecwrk/runs/'
 run_label = 'run00/'
 
-
+####
+# Make isolated directory for run:
 ####
 os.makedirs(test_dir + run_label + 'CHEMCLIM/', exist_ok = True)
 os.system("cp * " + test_dir + run_label + 'CHEMCLIM/')
+####
+# Autorun script
+####
+import 
+import 
