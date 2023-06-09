c2fc1ff42327ff6d5c2544fb329ca7609a2c5140
Joshua Parsons <mucephie@my.yorku.ca>
2023-06-09 11:40:43 -0400

    work on properly making a run and running it

--- CHEMCLIM/mk_run.py
+++ CHEMCLIM/mk_run.py
@@ -1,15 +1,19 @@
 import os
 import sys
 
-test_dir = '~/ecwrk/runs/'
-run_label = 'run00/'
-
+test_dir   = '/home/mucephie/ecwrk/runs/' # location for your runs
+run_label  = 'run00/'                     # label for run
+python     = 'vpython -m '                # your python command for desired python environment
+run_script = 'run_me.py'                  # the python script to initiate run
 ####
 # Make isolated directory for run:
 ####
 os.makedirs(test_dir + run_label + 'CHEMCLIM/', exist_ok = True)
-os.system("cp * " + test_dir + run_label + 'CHEMCLIM/')
+os.system("cp * " + test_dir + run_label + 'CHEMCLIM/ -r')
+
 ####
 # Autorun script
 ####
-import  test_dir + run_label + 'CHEMCLIM/run_test.py'
+os.chdir(test_dir + run_label + 'CHEMCLIM/')
+os.system(python + run_script)
+os.system('ls ')
